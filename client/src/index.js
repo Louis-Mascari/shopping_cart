02ab9ae0fc756f272ed4dbfb1caec3c149660d0a
Louis-Mascari <Lmascari16@gmail.com>
2024-02-04 20:50:59 -0500

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,4 @@
+// The starting point of the application
+// This is where the root component of the application - App - is rendered to the DOM. 
+// You can add new components to the /client/src/components folder and new test files to the /client/src/tests folder.
+
